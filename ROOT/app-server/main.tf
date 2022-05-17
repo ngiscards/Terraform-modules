8c8bcc213e32b54229656f27f841c9a7d43307bf
Giscard S Ngongang <ngiscards@users-MacBook-Pro.local>
2022-05-16 20:29:48 -0400

    creating app web

--- ROOT/app-server/main.tf
+++ ROOT/app-server/main.tf
@@ -3,10 +3,10 @@ module "vpc" {
   
 }
 
-module "vpc-dev" {
+module "app-dev" {
     source = "../../CHILD/VPC"
   vpc_cidr = "20.0.0.0/16"
     pub_cidr = "20.0.1.0/24"
     prv_cidr = "20.0.2.0/24"
-    tag = "dev"
+    tag = "app"
 }
